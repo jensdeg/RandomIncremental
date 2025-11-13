3000cb8924dd4c83a347f899b3a640831266ea0d
jensdeg <jensdegraaf88@gmail.com>
2025-11-13 02:16:29 +0100

    main loop

---
+++ src/RandomIncremental.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using RandomIncremental;
+
+Game gameloop = new()
+{
+    TickRate = 1000
+};
+
+gameloop.Start();
+
+Console.WriteLine("Game started. Press Enter to stop.");
+Console.ReadLine();
+gameloop.Stop();
---
+++ src/RandomIncremental/Game.cs
@@ -0,0 +1,37 @@
+﻿namespace RandomIncremental;
+
+public class Game
+{
+    public uint TickRate { get; set; } = 100;
+
+    private readonly List<ITickable> _tickables;
+    private Timer? _timer;
+
+    public Game()
+    {
+        _tickables = typeof(Game).Assembly
+            .GetTypes()
+            .Where(t => typeof(ITickable).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
+            .Select(t => (ITickable)Activator.CreateInstance(t)!)
+            .OrderBy(t => t.Priority)
+            .ToList() ?? [];
+    }
+
+    public void Start()
+    {
+        _tickables.ForEach(t => t.OnInitialized());
+        _timer = new Timer(_ => Tick(), null, 0, TickRate);
+    }
+
+    public void Stop() => _timer?.Dispose();
+
+    private void Tick() => _tickables.ForEach(t => t.OnTick());
+
+}
+
+public interface ITickable
+{
+    uint Priority { get; }
+    void OnTick();
+    virtual void OnInitialized() { }
+}
