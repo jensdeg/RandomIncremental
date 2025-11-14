7ebd7c0982b48e2b657f24a434ea32d01bef90ec
jensdeg <jensdegraaf88@gmail.com>
2025-11-14 01:14:25 +0100

    base gamestats initialization and no more interface

--- src/RandomIncremental/Game.cs
+++ src/RandomIncremental/Game.cs
@@ -3,35 +3,42 @@
 public class Game
 {
     public uint TickRate { get; set; } = 100;
+    public readonly GameStats GameStats = new();
 
-    private readonly List<ITickable> _tickables;
+    private readonly List<TickableBase> _tickables;
     private Timer? _timer;
 
     public Game()
     {
         _tickables = typeof(Game).Assembly
             .GetTypes()
-            .Where(t => typeof(ITickable).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
-            .Select(t => (ITickable)Activator.CreateInstance(t)!)
+            .Where(t =>
+                typeof(TickableBase).IsAssignableFrom(t) &&
+                !t.IsAbstract &&
+                t.IsClass)
+            .Select(t => (TickableBase)Activator.CreateInstance(t)!)
             .OrderBy(t => t.Priority)
             .ToList() ?? [];
+
+        foreach (var tickable in _tickables)
+        {
+            tickable.SetGameStats(GameStats);
+            tickable.OnInitialized();
+        }
     }
 
-    public void Start()
-    {
-        _tickables.ForEach(t => t.OnInitialized());
-        _timer = new Timer(_ => Tick(), null, 0, TickRate);
-    }
+    public void Start() => _timer = new Timer(_ => Tick(), null, 0, TickRate);
 
     public void Stop() => _timer?.Dispose();
 
     private void Tick() => _tickables.ForEach(t => t.OnTick());
-
 }
 
-public interface ITickable
+public abstract class TickableBase
 {
-    uint Priority { get; }
-    void OnTick();
-    virtual void OnInitialized() { }
+    protected GameStats GameStats { get; private set; } = null!;
+    public virtual uint Priority => 0;
+    public virtual void OnInitialized() { }
+    public abstract void OnTick();
+    public void SetGameStats(GameStats stats) => GameStats = stats;
 }
---
+++ src/RandomIncremental/GameStats.cs
@@ -0,0 +1,11 @@
+﻿using BreakInfinity;
+
+namespace RandomIncremental;
+
+public class GameStats
+{
+    public BigDouble Value { get; set; } = 0;
+
+    public BigDouble Max { get; set; } = 10;
+    public BigDouble Min { get; set; } = 0;
+}
